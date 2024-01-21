f1e681f8923c6d46f4ff0933d6dba0e3c7e2446f
Yann <yannleroy23@gmail.com>
2024-01-21 16:20:21 +0100

    Finish

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,6 +25,7 @@ export default function HomeScreen({ navigation }) {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email: email, password: password }),
         })
+        
     
           .then(response => response.json())
           .then(data => {
